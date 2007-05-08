e0335e8ec671f3240b12a8f0f11c5836662f2f20
mbbx6spp <mbbx6spp>
2007-05-08 00:47:52 +0000

    Fixed typo in update example code script

--- examples/update.rb
+++ examples/update.rb
@@ -1,8 +1,10 @@
+#!/usr/bin/env ruby
+
 require 'twitter'
 
 client = Twitter::Client.new(:login => 'mylogin', :password => 'mypassword')
 begin
   client.update('Twitter4R is so cool')
-rescue Twitter::RESTError, re
+rescue Twitter::RESTError => re
   puts re
 end

8c70db74691a69771414974063cd78fe462bc779
mbbx6spp <mbbx6spp>
2007-05-08 00:47:05 +0000

    Added Twitter::Client#send_direct_message example code for documentation

---
+++ examples/direct_message.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'twitter'
+
+client = Twitter::Client.new(:login => 'mylogin', :password => 'mypassword')
+begin
+  client.send_direct_message('friend_screen_name', 'Twitter4R is so cool, check it out at http://twitter4r.rubyforge.org')
+rescue Twitter::RESTError => re
+  puts re # perhaps try again also?  left as exercise.
+end

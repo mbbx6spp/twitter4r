4d3d5f37e6dd00c7bbfd75df537c22d3d7160368
mbbx6spp <mbbx6spp>
2007-05-08 00:39:12 +0000

    Fixed example script for update case again.  See #2 for details

--- examples/update.rb
+++ examples/update.rb
@@ -3,6 +3,6 @@ require 'twitter'
 client = Twitter::Client.new(:login => 'mylogin', :password => 'mypassword')
 begin
   client.update('Twitter4R is so cool')
-rescue RESTError, re
+rescue Twitter::RESTError, re
   puts re
 end

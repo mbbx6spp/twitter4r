f6ad9d4077e4e2c167e259ac985678541d594deb
mbbx6spp <mbbx6spp>
2007-07-08 14:20:54 +0000

    Added documentation

--- lib/twitter/console.rb
+++ lib/twitter/console.rb
@@ -3,8 +3,22 @@
 module Twitter
   class Client
     class << self
-      # Mostly helper method for irb shell prototyping
-      # TODO: move this out as class extension for twitter4r console script
+      # Helper method mostly for irb shell prototyping.
+      # 
+      # Reads in login/password Twitter credentials from YAML file
+      # found at the location given by <tt>config_file</tt> that has 
+      # the following format:
+      #  envname:
+      #    login: mytwitterlogin
+      #    password: mytwitterpassword
+      # 
+      # Where <tt>envname</tt> is the name of the environment like 'test', 
+      # 'dev' or 'prod'.  The <tt>env</tt> argument defaults to 'test'.
+      # 
+      # To use this in the shell you would do something like the following 
+      # examples:
+      #  twitter = Twitter::Client.from_config('config/twitter.yml', 'dev')
+      #  twitter = Twitter::Client.from_config('config/twitter.yml')
       def from_config(config_file, env = 'test')
         yaml_hash = YAML.load(File.read(config_file))
         self.new yaml_hash[env]

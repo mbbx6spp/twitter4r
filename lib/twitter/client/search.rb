a9cc03e737c1babb642837471078dc50de0cce02
Susan Potter <me@susanpotter.net>
2009-03-17 00:26:33 -0500

    Added basic Twitter Search API coverage

--- lib/twitter/client/search.rb
+++ lib/twitter/client/search.rb
@@ -1,20 +1,17 @@
 class Twitter::Client
 
   @@SEARCH_URIS = {
-    
+    :basic => "/search.json",
   }
 
   # Provides access to Twitter's Search API.
   # 
   # Example:
   #  # For keyword search
-  #  iterator = @twitter.search(:keyword => "coworking")
+  #  iterator = @twitter.search(:q => "coworking")
   #  while (tweet = iterator.next)
   #    puts tweet.text
   #  end
-  #  # For hashtag search
-  #  iterator = @twitter.search(:hashtag => "coworking") # or :hashtag => "#coworking"
-  #  iterator = @twitter.search(:
   # 
   # An <tt>ArgumentError</tt> will be raised if an invalid <tt>action</tt> 
   # is given.  Valid actions are:
@@ -22,10 +19,9 @@ class Twitter::Client
   # * +:sent+
   def search(options = {})
 #    raise ArgumentError, "Invalid messaging action: #{action}"
-    uri = @@SEARCH_URIS[action]
-    response = http_connect {|conn|	create_http_get_request(uri, options) }
-    bless_models(Twitter::Status.unmarshal(response.body))
+    uri = @@SEARCH_URIS[:basic]
+    response = http_connect(nil, false, :search) {|conn|	create_http_get_request(uri, options) }
+    json = JSON.parse(response.body)
+    bless_models(Twitter::Status.unmarshal(JSON.dump(json["results"])))
   end
-  
-  
 end

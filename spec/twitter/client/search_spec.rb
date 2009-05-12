83162a9ffcc672fdaffa8f4026634e9eedd96dd9
Susan Potter <me@susanpotter.net>
2009-05-11 22:20:01 -0500

    Added some search specs

--- spec/twitter/client/search_spec.rb
+++ spec/twitter/client/search_spec.rb
@@ -1,24 +1,68 @@
 require File.join(File.dirname(__FILE__), '..', '..', 'spec_helper')
 
-describe :rest_api_request, :shared => true do
+describe Twitter::Client, "#search" do
   before(:each) do
-    
-  end
-  it "should invoke #http_connect with expected arguments"
-  it "should create HTTP request of expected method"
-  it "should bless returned model of expected type"
-end
-
-describe Twitter::Client, "#end" do
-  describe "keywords case" do
-    def expected_http_method; :get; end
-    def expected_model; Twitter::Status; end
-    def expected_model_in_array?; true; end
-    def expected_http_connect_arguments; {}; end
-#    should_behave_like :rest_api_request
+    @twitter = client_context
+    @uris = Twitter::Client.class_eval("@@SEARCH_URIS")
+    @request = mas_net_http_get(:basic_auth => nil)
+    @response = mas_net_http_response(:success, "{\"results\": [], \"refresh_url\":\"?since_id=1768746401&q=blabla\"}")
+    @connection = mas_net_http(@response)
+    Net::HTTP.stub!(:new).and_return(@connection)
+    @statuses = []
+    Twitter::Status.stub!(:unmarshal).and_return(@statuses)
+    @page = 2
+    @keywords = "twitter4r"
+    @to = "SusanPotter"
+    @from = "twitter4r"
   end
   
-  describe "invalid" do
-    it "should raise an ArgumentError when giving an invalid search action"
+  it "should create expected HTTP GET request using :to" do
+    @twitter.should_receive(:create_http_get_request).with(@uris[:basic], {:to => @to}).and_return(@request)
+    @twitter.search(:to => @to)
+  end
+  
+  it "should bless the Array returned from Twitter for :to case" do
+    @twitter.should_receive(:bless_models).with(@statuses).and_return(@statuses)
+    @twitter.search(:to => @to)
+  end
+  
+  it "should create expected HTTP GET request using :from" do
+    @twitter.should_receive(:create_http_get_request).with(@uris[:basic], {:from => @from}).and_return(@request)
+    @twitter.search(:from => @from)
+  end
+  
+  it "should bless the Array returned from Twitter for :to case" do
+    @twitter.should_receive(:bless_models).with(@statuses).and_return(@statuses)
+    @twitter.search(:from => @from)
+  end
+  
+  it "should create expected HTTP GET request using :keywords" do
+    @twitter.should_receive(:create_http_get_request).with(@uris[:basic], {:keywords => @keywords}).and_return(@request)
+    @twitter.search(:keywords => @keywords)
+  end
+  
+  it "should bless the Array returned from Twitter for :keywords case" do
+    @twitter.should_receive(:bless_models).with(@statuses).and_return(@statuses)
+    @twitter.search(:keywords => @keywords)
+  end
+  
+  it "should accept paging option" do
+    lambda {
+      @twitter.search(:keywords => @keywords, :page => @page)
+    }.should_not raise_error(Exception)
+  end
+
+  it "should generate expected GET HTTP request for paging case" do
+    @twitter.should_receive(:create_http_get_request).with(@uris[:basic], {:page => @page}).and_return(@request)
+    @twitter.search(:page => @page)
+  end
+
+  it "should bless models for paging case" do
+    @twitter.should_receive(:bless_models).with(@statuses).and_return(@statuses)
+    @twitter.search(:page => @page)
+  end
+  
+  after(:each) do
+    nilize(@twitter, @uris, @request, @response, @connection, @statuses)
   end
 end

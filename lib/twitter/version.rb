59fdecc060995f393ff334e81d339c112aa242d2
Susan Potter <me@susanpotter.net>
2009-07-22 21:00:59 -0500

    Upped version and generated new gemspec for branch 0.4.0

--- lib/twitter/version.rb
+++ lib/twitter/version.rb
@@ -3,8 +3,8 @@
 
 module Twitter::Version #:nodoc:
   MAJOR = 0
-  MINOR = 3
-  REVISION = 1
+  MINOR = 4
+  REVISION = 0
   class << self
     # Returns X.Y.Z formatted version string
     def to_version

e4b5168717c61222dbdf24bb6c704cbfcba2e377
Susan Potter <me@susanpotter.net>
2009-11-21 18:14:15 -0600

    Upped the version number to 0.3.3

--- lib/twitter/version.rb
+++ lib/twitter/version.rb
@@ -4,13 +4,13 @@
 module Twitter::Version #:nodoc:
   MAJOR = 0
   MINOR = 3
-  REVISION = 2
+  REVISION = 3
   class << self
     # Returns X.Y.Z formatted version string
     def to_version
       "#{MAJOR}.#{MINOR}.#{REVISION}"
     end
-    
+
     # Returns X-Y-Z formatted version name
     def to_name
       "#{MAJOR}_#{MINOR}_#{REVISION}"
